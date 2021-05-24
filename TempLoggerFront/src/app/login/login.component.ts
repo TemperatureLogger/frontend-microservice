784f726a626338e5ad34f50c32010eb56e12c8f6
filimonadrian <adrian.filimon@hotmail.com>
2021-05-24 10:57:57 +0000

    Removed commented lines of code from login component

--- TempLoggerFront/src/app/login/login.component.ts
+++ TempLoggerFront/src/app/login/login.component.ts
@@ -21,10 +21,11 @@ export class LoginComponent implements OnInit {
 
 
   loginUser() :void {
-    let nameText = (<HTMLInputElement>document.getElementById('nameText')).value;//document.getElementById('nameText').value;
-    let passText = (<HTMLInputElement>document.getElementById('passText')).value;//document.getElementById('passText').value;
+    let nameText = (<HTMLInputElement>document.getElementById('nameText')).value;
+    let passText = (<HTMLInputElement>document.getElementById('passText')).value;
     let exists = false;
     // TODO: Replace the userDB with a get from the DB
+    // GET from database
     for (let i = 0; i < userDB.length; i++) {
       let user = userDB[i]
       console.log(user.name)
